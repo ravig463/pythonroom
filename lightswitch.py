d7298d665a28afad40dba044a90eb037b07a838e
ravig463 <ravigadgil123@gmail.com>
2014-04-20 15:35:43 -0700

    pythonroom update

--- lightswitch.py
+++ lightswitch.py
@@ -1,1 +1,24 @@
 # author: ravig463
+
+switch = False
+
+prisoners = range(1,101)
+
+rounds = range(1,101)
+# For every round
+for r in rounds:
+	# Go to every prisoner
+    for prisoner in prisoners:
+		# If the prisoner is a multiple of the round
+		if prisoner % r == 0:
+		    # Flip the switch
+			if switch == False:
+				switch = True
+			else:
+				switch = False
+				
+if switch == True:
+	print "Light is on"
+else:
+	print "Light is off"
+			
