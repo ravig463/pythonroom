07ee70ae674720b6d41f31f48952c6a807058d6e
ravig463 <ravigadgil123@gmail.com>
2014-04-27 13:47:31 -0700

    pythonroom update

--- food.py
+++ food.py
@@ -1,1 +1,11 @@
-# author: ravig463
+# author = ravig463
+
+computers = ["A", "B", "C", "D"]
+for computer in computers:
+	answer = input ( "Does computer " + computer + " have a virus?")
+	if answer == "yes":
+		print "Then debug " + computer
+	if answer == "no":
+	    print "Then computer " + computer + " is okay "
+	
+          
