3ec91d15a898217f572d9669911b3e251d9584ae
ravig463 <ravigadgil123@gmail.com>
2014-04-27 15:00:30 -0700

    pythonroom update

--- therealf13.py
+++ therealf13.py
@@ -1,3 +1,21 @@
 # author: ravig463
 
-total = 
+total = 0
+
+date = 1
+month = 1
+year = 1900
+
+while year < 2015:
+	total = total + 1
+	if month == 12 and date == 31:
+		year = year + 1
+		month = 1
+		date = 1
+		
+		
+	
+	else:
+		month = month + 1 
+	
+print total
