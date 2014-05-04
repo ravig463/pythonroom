2672bebbdfc64b7b036ea732c855c715c3acd0b4
ravig463 <ravigadgil123@gmail.com>
2014-05-04 15:40:00 -0700

    pythonroom update

--- askaquestion.py
+++ askaquestion.py
@@ -1,6 +1,33 @@
-question = input("Speak, human.")
+question = input("Ask me a specific question.")
 question = question.lower()
 word = question.split()
 
 if word[0] == "what":
-	if word[1] == "
+	if word[1] == "happened":
+		if word[2] == "to":
+			if word[3] == "you?":
+				print "I beat the Atlanta Braves"
+				
+if word[1] == "is":
+	if word[2] == "one":
+		if word[3] == "plus":
+			if word[4] == "two?":
+				print "Three"
+				
+if word[1] == "slapped":
+	if word[2] == "you":
+		if word[3] == "very":
+			if word[4] == "hard?":
+				print "A very muscular gorilla"
+				
+if word[1] == "in":
+	if word[2] == "the":
+		if word[3] == "world":
+			if word[4] == "is":
+				if word[5] == "lacrosse?":
+					print "A sport of Native American origin"
+			
+		
+
+
+	
