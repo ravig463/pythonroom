280a7eb70393f99c950c4074ff8816d4f276fc11
ravig463 <ravigadgil123@gmail.com>
2014-05-18 13:26:52 -0700

    pythonroom update

--- practice2.py
+++ practice2.py
@@ -1,1 +1,31 @@
 # author: ravig463
+
+import turtle 
+
+shape = input("What should I draw?")
+if shape == "square":
+	t = turtle.Turtle()
+	t.forward(100)
+	t.left(90)
+	t.forward(100)
+	t.left(90)
+    t.forward(100)
+	t.left(90)
+	t.forward(100)
+	t.left(90)
+if shape == "triangle":
+	t = turtle.Turtle()
+	t.forward(100)
+	t.left(120)
+	t.forward(100)
+	t.left(120)
+	t.forward(100)
+	t.left(120)
+	
+if shape == "circle":
+	t = turtle.Turtle()
+	for n in range(1, 121):
+		t.forward(3)
+		t.left(3)
+		
+	
