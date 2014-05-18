e3044afc8b3b51ec37688e7a8ae0818bd94cde39
ravig463 <ravigadgil123@gmail.com>
2014-05-18 15:05:24 -0700

    initial commit

---
+++ practice3.py
@@ -0,0 +1,22 @@
+# author: ravig463
+
+def sayHello():
+	print"Hello there!"
+	
+def sayHelloTo(name):
+	print "Hello " + name
+	
+def sayGreeting(greeting, name):
+	print greeting + " there, " + name
+	
+def sayGoodbye():
+	print "See ya later"
+	
+def sayGoodbyeTo(name):
+	print "todo"
+	
+def
+	
+	
+	
+	
